90f0dbf93ba04313bbdf791299de7052f78640d8
Nick Stone <35820605+Stonen2@users.noreply.github.com>
2018-04-08 21:12:59 -0400

    Add files via upload

---
+++ teaching/src/teaching/user.java
@@ -0,0 +1,32 @@
+package teaching;
+
+public class user
+{
+	private String name; 
+	private String email; 
+	
+	
+	public user(String inname, String inemail){
+		name = inname; 
+		email = inemail;
+		
+		
+	}
+	
+	public String getname(){
+		
+		return name; 
+		
+	}
+	
+	public String getemail() {
+		
+		
+		return email; 
+	}
+	
+	
+	
+	
+	
+}
