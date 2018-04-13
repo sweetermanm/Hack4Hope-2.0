617d678e337506b533e8b02cc2439b1cfd1b3d45
davisr18 <32375710+davisr18@users.noreply.github.com>
2018-04-13 07:53:35 -0700

    Add files via upload

---
+++ DeletePostFunction.java
@@ -0,0 +1,68 @@
+//createdby Roslyn
+package easy;
+public class DeletePostFunction {
+	private String username; 
+    private int postID; 
+    
+	
+	
+	public DeletePostFunction(String intitle, String incontent) {
+		title = intitle; 
+		content = incontent; //I kept this syntax
+		delete= null;
+
+	}
+	public DeletePostFunction(String userd, int inidd) {
+		
+		username = userd; //user delete
+		postID = idd; //post id delete
+		
+		
+		
+	}
+	public void setidd(int inpost) {
+		postID = inpost; 
+		
+	}
+	
+	
+	
+	public void setuserd(String inuse) {
+		username = inuse; 
+			
+	}
+	
+
+	public String getuserd() {
+		return username; 
+		
+	}
+	
+	public int getidd() {
+		return postID; 
+		
+	}
+
+	public String delete(){
+		return delete();
+	}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+<%
+
+%>
